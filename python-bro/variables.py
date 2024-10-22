255979b66b6b63f0298710a378ea5ec834fbf68a
ehiz <ehizomokhomion@gmail.com>
2024-10-23 00:00:16 +0100

    variable practice with bro_code

---
+++ python-bro/variables.py
@@ -0,0 +1,23 @@
+first_name = "Ehis"
+last_name = "Omokhomion"
+
+# integers
+
+userAge = 20
+dob = 2002
+
+# floats
+userGPA = 4.55
+gradYear = 2023
+
+# boolean
+isGrad = False
+
+
+print(f"Hello my first name is {first_name} and my last name {last_name}")
+
+print(f"I was born in the year {dob} and I am {userAge} years old")
+if isGrad:
+    print(f"I graduated in the year {gradYear} with a CGPA of {userGPA}")
+else:
+    print("I am still a student")
