495cbc7244fbaef89b42e86f1b436e456c8c18a7
ehiz <ehizomokhomion@gmail.com>
2024-10-25 00:00:57 +0100

    variables, print/input function, the switcheroo and bandname gen

---
+++ day-1/day-one.py
@@ -0,0 +1,21 @@
+# printing to console or something
+
+# print("hello booty!!")
+
+# input  function
+
+# input("What is your name")
+
+name = "Ehis Omokhomion"
+
+print(len(name))
+
+# the switcheroo
+
+a = 29
+b = 41
+c = a
+a = b
+b = c
+
+print(f"A is now '{a}' and b is now '{b}' ")
